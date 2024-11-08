ceefddd1db74aa531bd5abff72782913a95ed18b
guilhermemedeirosmergulhao <gui.mergulhao55@outlook.com>
2024-11-08 13:35:35 -0300

    Carregamento de imagem

--- users/register/js/register-photo-script.js
+++ users/register/js/register-photo-script.js
@@ -1,3 +1,20 @@
 function goToEmailAndPassword(){
     window.location.href = "register-email-password.html"
 }
+
+const photoPicker = document.getElementById("pick-photo")
+const profilePic = document.getElementById("profile-pic")
+
+photoPicker.addEventListener("change", function(){
+    const file = this.files[0]
+    if(file){
+        const reader = new FileReader()
+        reader.onload = function(event){
+            const base64image = event.target.result
+            localStorage.setItem("profilePic", base64image)
+            profilePic.src = base64image
+        }
+
+        reader.readAsDataURL(file)
+    }
+})
