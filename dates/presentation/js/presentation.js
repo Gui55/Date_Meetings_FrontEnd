eb95ae27e3b44a96ac3d4f98030655aaeddbe5a9
guilhermemedeirosmergulhao <gui.mergulhao55@outlook.com>
2024-11-17 06:51:45 -0300

    Alteração na cor do border da imagem de perfil dependendo do sexo do usuário

--- dates/presentation/js/presentation.js
+++ dates/presentation/js/presentation.js
@@ -9,6 +9,19 @@ if(sessionStorage.getItem("profileName")){
     profileName.innerText = sessionStorage.getItem("profileName")
 }
 
+if(sessionStorage.getItem("profileGender")){
+    gender = sessionStorage.getItem("profileGender")
+    let color = null
+    const rootStyles = getComputedStyle(document.documentElement)
+    if(gender=='masculino'){
+        color = rootStyles.getPropertyValue('--male-blue').trim()
+        profilePic.style.borderColor = color
+    } else if(gender=='outro'){
+        color = rootStyles.getPropertyValue('--other-green').trim()
+        profilePic.style.borderColor = color
+    }
+}
+
 function goToHome(){
     window.location.href = "../../../index.html"
 }
--- users/register/js/register-gender-script.js
+++ users/register/js/register-gender-script.js
@@ -1,3 +1,11 @@
 function goToPhoto(){
+    const radios = document.querySelectorAll('input[name=genero-sou]')
+
+    radios.forEach(radio => {
+        if(radio.checked){
+            sessionStorage.setItem("profileGender", radio.value)
+        }
+    })
+
     window.location.href = "register-photo.html"
 }
